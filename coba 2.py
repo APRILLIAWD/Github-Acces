390bde1bfb4ae9a697ddc67c22bc752dc99106de
APRILLIAWD <aprilliawd23@gmail.com>
2025-10-03 12:13:29 +0700

    Add files via upload

---
+++ coba 2.py
@@ -0,0 +1,12 @@
+# Menghitung luas persegi panjang
+
+panjang = float(input("Masukkan panjang:"))
+lebar = float(input("Masukkan lebar:"))
+
+
+luas_persegi_panjang =panjang * lebar
+print("luas persegi panjang adalah {}".format(luas_persegi_panjang))
+
+                    
+                    
+            
